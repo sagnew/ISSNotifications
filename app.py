271262d4c1bd9791b688676a6b616f90103558fc
Sam Agnew <sagnew@eden.rutgers.edu>
2015-10-30 22:39:10 -0400

    Add basic html templates for the subscribers page

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template()
+    return render_template('index.html')
 
 
 @app.route('/subscribe', methods=['POST'])
@@ -17,6 +17,6 @@ def subscribe():
     lat = request.form['latitude']
     lon = request.form['longitude']
     iss.add_to_queue(number, lat, lon)
-    return render_template()
+    return render_template('subscribed.html')
 
 app.run(host='0.0.0.0')
