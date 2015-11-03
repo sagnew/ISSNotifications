c3537fae9eb88e3c12adc922fd5a6f123fbcf809
Sam Agnew <sagnew@eden.rutgers.edu>
2015-11-02 19:26:49 -0500

    Return string upon subscription instead of another web page

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ def subscribe():
     lat = request.form['latitude']
     lon = request.form['longitude']
     iss.add_to_queue(number, lat, lon)
-    return render_template('subscribed.html')
+    return 'Thanks for subscribing. ' \
+           'Expect to receive notifications whenever the ISS flies over you!'
 
 app.run(host='0.0.0.0', debug=True)
