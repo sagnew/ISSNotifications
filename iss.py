513750c4e1b3aaced844cf6507c52fe86c4c7e47
Sam Agnew <sagnew@eden.rutgers.edu>
2015-11-03 01:05:15 -0500

    Make code python 3 compatible and reformat strings

--- iss.py
+++ iss.py
@@ -21,35 +21,37 @@ def get_next_pass(lat, lon):
 
     next_pass = response['response'][0]['risetime']
 
-    print 'Next pass is: ' + str(datetime.fromtimestamp(next_pass))
+    print('Next pass is: {}'.format(datetime.fromtimestamp(next_pass)))
     return datetime.fromtimestamp(next_pass)
 
 
-def add_to_queue(number, lat, lon):
+def add_to_queue(phone_number, lat, lon):
     # Add this phone number to Redis associated with "lat,lon"
-    redis_server.set(number, str(lat) + ',' + str(lon))
+    redis_server.set(phone_number, '{},{}'.format(lat, lon))
 
     # Get the timestamp of the next ISS flyby for this number.
     next_pass_timestamp = get_next_pass(lat, lon)
 
     # Schedule a text to be sent at the time of the next flyby.
-    scheduler.enqueue_at(next_pass_timestamp, notify_subscriber, number)
+    scheduler.enqueue_at(next_pass_timestamp, notify_subscriber, phone_number)
 
-    print str(number) + ' has been added to the queue at ' \
-        + str(lat) + ', ' + str(lon)
+    print('{} will be notified when ISS passes by {}, {}'
+          .format(phone_number, lat, lon))
 
 
-def notify_subscriber(number):
+def notify_subscriber(phone_number):
     msg_body = 'Look up! You may not be able to see it, but the International' \
                ' Space Station is passing above you right now!'
 
     # Retrieve the latitude and longitude associated with this number.
-    lat, lon = redis_server.get(number).split(',')
+    lat, lon = redis_server.get(phone_number).split(',')
 
     # Send a message to the number alerting them of an ISS flyby.
-    client.messages.create(to=number, from_='A_TWILIO_NUMBER', body=msg_body)
+    client.messages.create(to=phone_number,
+                           messaging_service_sid='MESSAGING_SERVICE_SID',
+                           body=msg_body)
 
     # Add the subscriber back to the queue to receive their next flyby message.
-    add_to_queue(number, lat, lon)
+    add_to_queue(phone_number, lat, lon)
 
-    print 'Message has been sent to ' + number
+    print('Message has been sent to {}'.format(phone_number))
